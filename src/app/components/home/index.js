1ffc1dfc3ed4d1478d6c868b2cce8e493f175e99
Deepesh Pathak <deepshpathak@gmail.com>
2017-06-12 22:36:29 +0530

    Complete initial setup start work on header

---
+++ src/app/components/home/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+	render() {
+		return(
+			<div className="main">
+				<h1> Home </h1>
+			</div>
+			);
+	}
+}
+
+export default Home;
--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,10 @@
-alert("it works fine !!!");
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, Route, browserHistory} from 'react-router';
+
+import Home from './components/home/';
+
+// Render the App Component
+render( <Router history={browserHistory}>
+            <Route path='/' component={Home}/>
+        </Router>, document.getElementById("app"));
